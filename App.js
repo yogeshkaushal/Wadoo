b375633e17a58a3864d0c93838fb48736309be39
yogeshkaushal <yogeshkshl.yk@gmail.com>
2022-01-22 20:18:24 -0500

    Added Google Login in Android/iOS

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {LogBox} from 'react-native';
-import {store} from './src/store';
-import {Provider} from 'react-redux';
+import { LogBox } from 'react-native';
+import { store } from './src/store';
+import { Provider } from 'react-redux';
 import RouteNavigator from './src';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\n're using an old API with gesture components, check out new Gestures system!",
 ]);
+
+GoogleSignin.configure({
+  scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+  webClientId: '582761720891-p4lit86r21lr5s2kvvdsf3p9iprn3e5e.apps.googleusercontent.com',
+  offlineAccess: true,
+  hostedDomain: '',
+  loginHint: '',
+  forceConsentPrompt: true,
+  accountName: '',
+  iosClientId: '582761720891-ib0ofj26hv7nk1dlo4tl06n7eeogmt4v.apps.googleusercontent.com'
+});
 
 const App = () => {
   return (
--- src/components/screens/LoginScreen.js
+++ src/components/screens/LoginScreen.js
@@ -1,48 +1,76 @@
 import React from 'react';
 import {
-  View,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
+    View,
+    StyleSheet,
+    SafeAreaView,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/core';
 
 const LoginScreen = () => {
-  return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <View style={styles.conatiner}>
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.textStyle}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+
+    const navigation = useNavigation();
+
+    const onGoogleButtonPress = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+
+            const googleCredential = auth.GoogleAuthProvider.credential(userInfo.idToken);
+
+            navigation.navigate('Tabs');
+            return auth().signInWithCredential(googleCredential);
+
+
+        } catch (error) {
+            console.log(error, "ERROR")
+        }
+    };
+
+    return (
+        <SafeAreaView style={styles.safeAreaView}>
+            <View style={styles.conatiner}>
+                <GoogleSigninButton
+                    style={{ width: 192, height: 48 }}
+                    size={GoogleSigninButton.Size.Wide}
+                    color={GoogleSigninButton.Color.Dark}
+                    onPress={onGoogleButtonPress} />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  conatiner: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  safeAreaView: {
-    flex: 1,
-  },
-  textStyle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  buttonStyle: {
-    backgroundColor: 'red',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    width: '90%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 7,
-    marginVertical: 10,
-  },
+    conatiner: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    safeAreaView: {
+        flex: 1,
+    },
+    textStyle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    buttonStyle: {
+        backgroundColor: 'red',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        width: '90%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 7,
+        marginVertical: 10,
+    },
 });
 
 export default LoginScreen;
